fedd335836f51f42a7876d2b46d8094b2c581872
Mihir Patel <146045697+Saints41@users.noreply.github.com>
2023-12-20 18:13:37 -0500

    Add files via upload
    Only the title screen with a play button is added, no actual game has been implemented but will work on that soon

---
+++ PersonalProjects/HTML_and_WebsiteDevelopment/game/pages/title.js
@@ -0,0 +1,13 @@
+$(document).ready(function($){
+
+    /*$("#btnPlay").click(function(event){
+		bg=$("div.bgI");
+        btn = $("a.btnPlay");
+		bg.animate({opacity:0},"slow");
+        btn.animate({opacity:0},"slow");
+        $("#btnPlay").prop("disabled",false);
+
+
+	});*/
+
+});
